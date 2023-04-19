9364e826a8eaf10091655cc748930cca8668903f
Coca162 <coca16622@gmail.com>
2023-04-19 12:38:54 +0100

    Replace wgpu with softbuffer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+#![warn(clippy::all, clippy::nursery)]
 
 use copypasta_ext::{prelude::ClipboardProvider, x11_bin::ClipboardContext};
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-use iced_winit::winit::event_loop::EventLoop;
+use winit::event_loop::EventLoop;
 
 pub mod window_pickers;
 
@@ -12,19 +12,23 @@ use window_pickers::get_color;
 fn main() {
     let mut event_loop = EventLoop::new();
 
-    let (r, g, b) = get_color(&mut event_loop).unwrap();
+    loop {
+        let (r, g, b) = get_color(&mut event_loop).unwrap();
 
-    let rgb_hex = format!("#{r:02X}{g:02X}{b:02X}");
+        let rgb_hex = format!("#{r:02X}{g:02X}{b:02X}");
 
-    print_result((r, g, b), &rgb_hex);
+        print_result((r, g, b), &rgb_hex);
 
-    let clip_res = ClipboardContext::new().and_then(|mut x| x.set_contents(rgb_hex));
+        let clip_res = ClipboardContext::new().and_then(|mut x| x.set_contents(rgb_hex));
 
-    if clip_res.is_err() {
-        println!("Failed to set clipboard content (do you have xclip?)");
+        if clip_res.is_err() {
+            println!("Failed to set clipboard content (do you have xclip?)");
+        }
+
+        println!("bruh!");
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
-
-    println!("bruh!");
 }
 
 fn print_result((r, g, b): (u8, u8, u8), rgb_hex: &str) -> Option<()> {
--- src/window_pickers/mod.rs
+++ src/window_pickers/mod.rs
@@ -1,17 +1,18 @@
-use iced_winit::winit::{
+use image::{GenericImageView, ImageFormat, DynamicImage};
+use screenshots::Screen;
+use softbuffer::GraphicsContext;
+use std::collections::HashMap;
+use winit::{
     dpi::PhysicalPosition,
-    event::{ElementState, Event, MouseButton, WindowEvent},
+    event::{ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent},
     event_loop::EventLoop,
     platform::run_return::EventLoopExtRunReturn,
     window::{Fullscreen, Window, WindowBuilder, WindowId},
 };
-use image::{GenericImageView, ImageFormat};
-use screenshots::{Image, Screen};
-use std::collections::HashMap;
 
 #[allow(clippy::must_use_candidate)]
 pub fn get_color(event_loop: &mut EventLoop<()>) -> Option<(u8, u8, u8)> {
-    let (_windows, map) = monitor_windows_with_screenshots(event_loop)?;
+    let (mut windows, mut map) = monitor_windows_with_screenshots(event_loop)?;
 
     let mut position = None;
 
@@ -47,21 +48,47 @@ pub fn get_color(event_loop: &mut EventLoop<()>) -> Option<(u8, u8, u8)> {
 
                 println!("{position:?}");
 
-                let image = &map[&window_id];
+                let (_, image) = &map[&window_id];
                 colors = Some(get_pixel(image, position));
 
+                windows.clear();
+
                 control_flow.set_exit();
+            }
+            Event::WindowEvent {
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                ..
+            } => {
+                windows.clear();
+            }
+            Event::RedrawRequested(window_id) => {
+                let (ctx, image) = map.get_mut(&window_id).unwrap();
+
+                let buffer: Vec<u32> = image.as_rgba8().unwrap().chunks(4).map(|rgb|
+                    rgb[2] as u32 | ((rgb[1] as u32) << 8) | ((rgb[0] as u32) << 16)
+                ).collect();
+                
+                ctx.set_buffer(&buffer, image.width() as u16, image.height() as u16);
             }
             _ => (),
         }
     });
-
     colors
 }
 
 fn monitor_windows_with_screenshots(
     event_loop: &EventLoop<()>,
-) -> Option<(Vec<Window>, HashMap<WindowId, Image>)> {
+) -> Option<(Vec<Window>, HashMap<WindowId, (GraphicsContext, DynamicImage)>)> {
     let screens = Screen::all().ok()?;
 
     let monitors: HashMap<_, _> = event_loop
@@ -79,19 +106,30 @@ fn monitor_windows_with_screenshots(
         .map(|builder| builder.build(event_loop).ok())
         .collect::<Option<_>>()?;
 
-    let images: Vec<Image> = screens
+    let images: Vec<DynamicImage> = screens
         .iter()
-        .map(|x| x.capture().ok())
-        .collect::<Option<_>>()?;
+        .map(|screen| screen.capture().ok().unwrap())
+        .map(|image| image::load_from_memory_with_format(image.buffer(), ImageFormat::Png).unwrap())
+        .collect();
 
-    let map = windows.iter().map(Window::id).zip(images).collect();
+    let map = windows
+        .iter()
+        .zip(images)
+        .map(|(window, image)| {
+            (
+                window.id(),
+                (
+                    unsafe { GraphicsContext::new(&window, &window) }.unwrap(),
+                    image,
+                ),
+            )
+        })
+        .collect();
 
     Some((windows, map))
 }
 
-fn get_pixel(image: &Image, position: PhysicalPosition<u32>) -> (u8, u8, u8) {
-    let image = image::load_from_memory_with_format(image.buffer(), ImageFormat::Png).unwrap();
-
+fn get_pixel(image: &DynamicImage, position: PhysicalPosition<u32>) -> (u8, u8, u8) {
     let pixel = image.get_pixel(position.x, position.y).0;
 
     (pixel[0], pixel[1], pixel[2])
